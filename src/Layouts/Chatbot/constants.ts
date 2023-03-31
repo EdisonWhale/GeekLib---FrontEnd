9239ae0b9a86c60a2c752eea2d9f8b571298703f
EdisonWhale <103423072+EdisonWhale@users.noreply.github.com>
2023-03-31 05:07:26 -0400

    Update constants.ts

--- src/Layouts/Chatbot/constants.ts
+++ src/Layouts/Chatbot/constants.ts
@@ -1,4 +1,4 @@
-export const API_KEY = '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'
+export const API_KEY = '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'
 export const systemMessage = {
   role: 'system',
   content: "Hello, I'm GeekLib AIChatbot! Ask me anything!",
