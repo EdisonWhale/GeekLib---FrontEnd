899891dbe723d39faa2eda2b72b5d69d6f3421c9
EdisonWhale <103423072+EdisonWhale@users.noreply.github.com>
2023-03-31 04:57:39 -0400

    Update oktaConfig.ts

--- src/Lib/oktaConfig.ts
+++ src/Lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oa8gawls3LJ3Dg9y5d7',
     issuer: 'https://dev-62669696.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:3000/login/callback',
+    redirectUri: 'https://www.geeklibr.com/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
